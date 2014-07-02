e5484ba35a50b1ba1d9f458bb8f314323ae02d57
Kevin Ali <kevinali1@yahoo.com>
2014-07-02 19:17:28 -0400

    Fixed but in convert

--- convert.py
+++ convert.py
@@ -10,6 +10,9 @@ for filename in filenames:
     input_filename = "'" + str(filename) +"'"
     input_filename_no_extension = filename[4:-4]
     
+    # Convert file to txt
+    output_filename =  "out/" + slugify(unicode(input_filename_no_extension)) + ".txt"
+    os.system("pdftotext " + input_filename + " " + output_filename)
     
     # Convert file to slugified filename
     new_input_name = 'temp/' + slugify(unicode(input_filename_no_extension)) + ".pdf"
@@ -17,6 +20,4 @@ for filename in filenames:
     #print command
     os.system(command)
     
-    # Convert file to txt
-    output_filename =  "out/" + slugify(unicode(input_filename_no_extension)) + ".txt"
-    os.system("pdftotext " + input_filename + " " + output_filename)
+
